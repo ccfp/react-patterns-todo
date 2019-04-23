163b9482a41ab5f4846f4b5fb6e145af851e6bc8
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-22 23:18:19 -0400

    Clarify function##map implementation

--- src/scratch.2.js
+++ src/scratch.2.js
@@ -10,7 +10,7 @@ const map = fn => arr => arr.map(fn)
 map(num => num + 1)(arr)
 //-> [2, 3, 4, 5, 6]
 
-// Drawing a parallel between Array##map and Promise##resolve
+// Drawing a parallel between Array##map and Promise##then
 Array.of(2).map(num => num + 1)
 //-> same as Array.of(3)
 Promise.resolve(2).then(num => num + 1)
@@ -36,13 +36,16 @@ Box(2)
   .fold()
 //-> 6
 
+// map :: F a ~> (a -> b) -> F b
+// map :: (r -> a) ~> (a -> b) -> r -> b
 // This still blows my mind a little bit, but
 // it allows us to map over the returned value
 // of a function, before we actually call that
 // function
 Function.prototype.map = function(AtoB) {
-  return C => {
-    const A = this(C)
+  return R => {
+    const RtoA = this
+    const A = RtoA(R)
     const B = AtoB(A)
     return B
   }
@@ -52,7 +55,8 @@ const increment = num => num + 1
 
 const incrementWithMessage = increment.map(
   n => `Heres the incremented number ${n}`
-)(3)
+)
+incrementWithMessage(3)
 //-> "Here's the incremented number 4"
 
 const add = x => y => x + y
